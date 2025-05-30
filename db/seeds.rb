00e16fd2c82f418873369c4641fe6bcac9b4fef6
Valentin Sala <valmax.sala@gmail.com>
2025-05-30 09:14:59 +0200

    debut front

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+list1 = List.create!(name: "Mes films d'action préférés")
+list2 = List.create!(name: "Comédies à voir entre amis")
+
+
+movies = Movie.limit(5)
+
+
+Bookmark.create!(list: list1, movie: movies[0], comment: "Un classique !")
+Bookmark.create!(list: list1, movie: movies[1], comment: "À revoir absolument")
+Bookmark.create!(list: list2, movie: movies[2], comment: "Fou rire garanti")
+Bookmark.create!(list: list2, movie: movies[3], comment: "À regarder en famille")
+
+puts "Deux listes et leurs bookmarks ont été créés avec succès !"
