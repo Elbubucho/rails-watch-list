5be658b53fd3acf88ff2fe3e11276aeaefb30266
Valentin Sala <valmax.sala@gmail.com>
2025-05-30 12:05:43 +0200

    Mise à jour de la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,7 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-list1 = List.create!(name: "Mes films d'action préférés")
-list2 = List.create!(name: "Comédies à voir entre amis")
-
-
-movies = Movie.limit(5)
-
-
-Bookmark.create!(list: list1, movie: movies[0], comment: "Un classique !")
-Bookmark.create!(list: list1, movie: movies[1], comment: "À revoir absolument")
-Bookmark.create!(list: list2, movie: movies[2], comment: "Fou rire garanti")
-Bookmark.create!(list: list2, movie: movies[3], comment: "À regarder en famille")
-
-puts "Deux listes et leurs bookmarks ont été créés avec succès !"
+Movie.create!(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create!(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create!(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create!(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
